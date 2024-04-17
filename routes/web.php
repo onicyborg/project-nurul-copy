2deb071843bba1b9a724232d153e955a9b729662
Ace <akhmadfauzy40@gmail.com>
2024-04-17 08:29:59 +0700

    menambahkan middleware

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+//routes untuk hrd
+Route::group(['middleware' => 'role:hrd'], function () {
+
+});
+
+//routes untuk kandidat
+Route::group(['middleware' => 'role:kandidat'], function () {
+
+});
